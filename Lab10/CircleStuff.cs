f856c52bedac962db5890339ae2d87ae20896683
Anthony Todek <tonytodek@yahoo.com>
2018-10-23 15:20:39 -0400

    first commit

---
+++ Lab10/CircleStuff.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10
+{
+    class Circle
+    {
+        public const double PI = Math.PI;
+        private double radius;
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public string FormatNumber(double x)
+        {
+            return $"{x,0:N2}";
+        }
+
+        public double CalculateCircumference()
+        {
+            double circumference = PI * (radius * 2);
+            return circumference;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+
+        public double CalculateArea()
+        {
+            double area = PI * (radius * radius);
+            return area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = FormatNumber(CalculateArea());
+            return formattedArea;
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab10
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(" Please enter the radius of a circle!");
+            double userInput = double.Parse(Console.ReadLine());
+
+            Circle myCircle = new Circle(userInput);
+
+            Console.WriteLine($"Circumference: {myCircle.CalculateCircumference()}");
+            Console.WriteLine($"Circumference Formatted! {myCircle.CalculateFormattedCircumference()}");
+
+            Console.WriteLine($"Area: {myCircle.CalculateArea()}");
+            Console.WriteLine($"Area: {myCircle.CalculateFormattedArea()}");
+        }
+    }
+}
